e551a3536be8403d4017da910c074c9a44ba77e0
Bucol <37924597+bucol@users.noreply.github.com>
2023-01-27 20:55:12 +0700

    Update botcomment.js

--- commands/creator/botcomment.js
+++ commands/creator/botcomment.js
@@ -4,7 +4,7 @@ module.exports = {
     use: "<query>",
     desc: "Bot Comment Maker",
     type: "creator",
-    example: "%prefix%command <query>",
+    example: "Contoh %prefix%command bucol ganteng",
     start: async(killua, m, { text }) => {
         killua.sendFile(m.from, global.api("zenz", "/creator/botcomment", {text: text}, "apikey"), "", m)
     },
